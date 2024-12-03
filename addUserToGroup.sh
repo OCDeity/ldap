9216722ed3153d60c17d0279247a73b5298e6f06
kopper42 <kopper42@gmail.com>
2024-12-03 11:38:31 +0000

    Added result checking and more verbose output.

--- addUserToGroup.sh
+++ addUserToGroup.sh
@@ -13,48 +13,59 @@ if [ -z "$BASE_DN" ]; then
 fi
 
 
-
+echo "Add User to Group"
 
 # Ask for the username if 
 if [ -z "$USERNAME" ]; then
-    read -p "Username: " USERNAME
+    read -p "  Username: " USERNAME
+else
+    echo "  Username: $USERNAME"
 fi
 
 # Check to see if the user exists:
 result=$(ldapUserExists "$USERNAME" "$BASE_DN")
 if ! [ "$result" == "true" ]; then
-    echo "The user ${USERNAME} was not found"
+    echo "  User \"${USERNAME}\" not found!"
     exit 0
 fi
 
 
 # Ask for the group name if it's not already set:
 if [ -z "$GROUPNAME" ]; then
-    read -p "Group: " GROUPNAME
+    read -p "  Group: " GROUPNAME
+else
+    echo "  Group: $GROUPNAME"
 fi
 
 # Get the group DN:
 GROUP_DN=$(ldapGetGroupDN "$GROUPNAME" "$BASE_DN")
 if ! [ -n "$GROUP_DN" ]; then
-    echo "The group \"$GROUPNAME\" was not found."
+    echo "  Group \"$GROUPNAME\" not found!"
     exit 0
 fi
+
 
 # Check to see if the user is already a member of the group:
 result=$(ldapIsMember "$GROUPNAME" "$USERNAME" "$BASE_DN")
 if [ "$result" == "true" ]; then
-    echo "The user \"$USERNAME\" is already part of the group \"$GROUPNAME\""
+    echo "  User \"$USERNAME\" already in \"$GROUPNAME\""
     exit 0
 fi
 
 
 # Create a temporary file with a unique name
 temp_file=$(mktemp)
+echo "  Temp file: $temp_file"
 
 # Export all of the variables we've collected and use them for templating
 export BASE_DN USERNAME GROUP_DN
 TEMPLATE_FILE=$(realpath "${TEMPLATE_PATH}/AddUserToGroup.txt")
 envsubst < "$TEMPLATE_FILE" > "$temp_file"
 
+getLDAPPassword LDAP_PASSWORD
+
 # Attempt to execute the modification in the .ldiff file we just constructed:
-ldapModify "$temp_file" "$BASE_DN" "$LDAP_PASSWORD"
+result=$(ldapModify "$temp_file" "$BASE_DN" "$LDAP_PASSWORD" 2>/dev/null)
+verifyResult "$?" "$result"
+
+echo "  User \"$USERNAME\" added to group \"$GROUPNAME\""
