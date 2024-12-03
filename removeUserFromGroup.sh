2454723235879e9d2d12a3e6fa0cb38a7e29a6ed
kopper42 <kopper42@gmail.com>
2024-12-03 11:48:19 +0000

    Added result checking and more verbose output

--- removeUserFromGroup.sh
+++ removeUserFromGroup.sh
@@ -11,50 +11,56 @@ if [ -z "$BASE_DN" ]; then
     BASE_DN=$(getBaseDN)
 fi
 
+echo "Remove User from Group"
+
 # If we don't have a username, ask for one
 if [ -z "$USERNAME" ]; then
-    read -p "Username: " USERNAME
+    read -p "  Username: " USERNAME
+else
+    echo "  Username: $USERNAME"
 fi
 
 result=$(ldapUserExists "$USERNAME" "$BASE_DN")
 if ! [ "$result" == "true" ]; then
-    echo "The user ${USERNAME} was not found"
+    echo "  User \"${USERNAME}\" not found!"
     exit 0
 fi
 
 # Ask for the group name if we don't have it
-if [ -z "$GROUPNAME"]; then
-    read -p "Group: " GROUPNAME
+if [ -z "$GROUPNAME" ]; then
+    read -p "  Group: " GROUPNAME
+else
+    echo "  Group: $GROUPNAME"
 fi
 
 GROUP_DN=$(ldapGetGroupDN "$GROUPNAME" "$BASE_DN")
 if ! [ -n "$GROUP_DN" ]; then
-    echo "The group \"$GROUPNAME\" was not found."
+    echo "  Group \"$GROUPNAME\" not found!"
     exit 0
 fi
 
 # Check to see if the user is already a member of the group
-result=$(ldapIsMember "$GROUPNAME" "$USERNAME" "$BASE_DN")
+result=$(ldapIsMember "$GROUPNAME" "$USERNAME" "$BASE_DN" 2>/dev/null)
+verifyResult "$?" "$result"
+
 if ! [ "$result" == "true" ]; then
-    echo "The user \"$USERNAME\" is not part of the group \"$GROUPNAME\""
+    echo "  User \"$USERNAME\" not in \"$GROUPNAME\""
     exit 0
 fi
 
-echo "Removing the user \"$USERNAME\" from the group \"$GROUPNAME\"."
-
 # Create a temporary file with a unique name
 temp_file=$(mktemp)
+echo "  Temp ldif: $temp_file"
 
 # Export all of the variables we've collected and use them for templating
 export BASE_DN USERNAME GROUP_DN
 TEMPLATE_FILE=$(realpath "${TEMPLATE_PATH}/RemoveUserFromGroup.txt")
 envsubst < "$TEMPLATE_FILE" > "$temp_file"
 
-# If we don't have the LDAP Password, ask for it.
-if [ -z "$LDAP_PASSWORD" ]; then
-    read -s -p "LDAP Admin Password: " LDAP_PASSWORD
-    echo ""
-fi
+getLDAPPassword LDAP_PASSWORD
 
 # Attempt to execute the modification in the .ldiff file we just constructed:
-ldapModify "$temp_file" "$BASE_DN" "$LDAP_PASSWORD"
+result=$(ldapModify "$temp_file" "$BASE_DN" "$LDAP_PASSWORD" 2>/dev/null)
+verifyResult "$?" "$result"
+
+echo "  User \"$USERNAME\" removed from group \"$GROUPNAME\""
