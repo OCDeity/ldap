9ed72d06fa25f62e6c5ab785241b0bf3e2c86ea8
kopper42 <kopper42@gmail.com>
2024-12-05 00:47:59 +0000

    Fixed error when no OUs exist.  Fixed entity count.  Added OU count.

--- showOUs.sh
+++ showOUs.sh
@@ -20,14 +20,29 @@ if [ ${#result[@]} -gt 0 ]; then
     # Build up a string of tab delimited output.
     # We start with the headers:
     OU_DATA="Entities\tOrganizational_Unit\n"
+    OU_COUNT=0
     while IFS= read -r org_unit; do
+
+        # Skip empty lines:
+        if [ -z "$org_unit" ]; then
+            continue
+        fi
+
+        # Get the count of members in the OU:
         count=$(ldapGetOUMemberCount "$org_unit" "$BASE_DN")
         verifyResult "$?" "$count"
 
+        # The count includes itself, so we need to subtract one:
+        count=$(($count - 1))
+
         OU_DATA+="${count}\t${org_unit}\n"
+        OU_COUNT=$((OU_COUNT + 1))
     done <<< "$result"
 
-    echo "Organizational Units found in \"$BASE_DN\":"
-    echo -e "$OU_DATA" | column -t
+    # Only show the data if we have any:
+    if [ $OU_COUNT -gt 0 ]; then
+        echo "Organizational Units found in \"$BASE_DN\" ($OU_COUNT):"
+        echo -e "$OU_DATA" | column -t
+    fi
 fi
 
